dfce042fdd90705ec8f8aa42b5bf095c43e876c4
bstorozhuk <storozhuk.b.m@gmail.com>
2020-09-15 17:59:22 +0100

    Improve godoc.

--- pcache.go
+++ pcache.go
@@ -10,7 +10,8 @@ type cacheStripe struct {
 	cache cache
 }
 
-// PCache is safe for concurrent use, goroutine local cache.
+// PCache is safe for concurrent use cache that tries to keep data local for the goroutine
+// and reduce synchronization overhead.
 // All operations run in amortized constant time.
 // PCache does its best to cache items inside and do as little synchronization as possible
 // but since it is cache, there is no guarantee that PCache won't evict your item after Store.
